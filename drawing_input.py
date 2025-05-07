cf956e39e9389560d9296d0a6ecf59b5c68815be
JozMroz <praca.jozefmroZ@GMAIL.COM>
2025-05-07 21:02:46 +0200

    Update drawing_input.py

--- drawing_input.py
+++ drawing_input.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 
-def drawing_input(x, y, signal_type, ylabel='Output [-]'):
+def drawing_input(x, y, signal_type, ylabel='Input [-]'):
     plt.plot(x, y)
     plt.xlabel('Time [s]')
     plt.ylabel(ylabel)
 
-    if signal_type == 'fun.sin_Tm':
-        plt.title('Response to Sinus Input')
+    if signal_type == 'sinus':
+        plt.title('Sinus function')
     elif signal_type == 'triangle':
-        plt.title('Response to Triangle Input')
-    elif signal_type == 'sinus':
-        plt.title('Response to Sinusoidal Input')
+        plt.title('Triangle function')
+    elif signal_type == 'rect':
+        plt.title('Rectangular function')
+    elif signal_type == 'rect_imp':
+        plt.title('Rectangular impulse function')
+    elif signal_type == 'trian_imp':
+        plt.title('Triangular impulse function')
     else:
         plt.title('System Response')
     plt.show()
